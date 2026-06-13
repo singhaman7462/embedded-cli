99b8bf77f0b743433244242c7164dbf70d0aa9b2
Aman Kumar Singh <amans.ug21.ec@nitp.ac.in>
2026-06-14 02:33:38 +0530

    Transmitted character from PC Serial Monitor to ESP32

--- firmware/UART_startup/uart_startup.ino
+++ firmware/UART_startup/uart_startup.ino
@@ -2,9 +2,24 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("UART Initialized successfully");
+
+ 
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(10); // this speeds up the simulation
+
+ volatile int countInBuffer = Serial.available();
+    String userInput = "";
+    if (countInBuffer > 0) {
+      for (int i = 0; i < countInBuffer; i++) {
+        char currentCharacter=Serial.read();
+        userInput +=currentCharacter;
+      }
+    }
+    Serial.println(userInput);
+
+  delay(3000); // this speeds up the simulation
+
 }
